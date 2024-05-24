4f16895bf90fd8c4118343e4d779ff0d1ffc0df3
Martin McGloin <martin.mcgloin@airthings.com>
2024-05-24 11:11:43 +0200

    Removed the query string param based on PR feedback

--- src/constants/Fieldwire.constants.ts
+++ src/constants/Fieldwire.constants.ts
@@ -1,3 +1,3 @@
 export const FIELDWIRE_CONSTANTS = {
-    getProjectUrl: (projectId: string) => `https://app.fieldwire.com/#!/projects/${projectId}/plans?lang=en`,
+    getProjectUrl: (projectId: string) => `https://app.fieldwire.com/#!/projects/${projectId}/plans`,
 }
