2c09e632c8547881a4e7c02e0e8e7dc3fe574154
anaviz <ana.vizcarrondo@gmail.com>
2024-05-30 13:25:46 +0200

    adds Export All button to Task List View

--- src/pages/Tasks.jsx
+++ src/pages/Tasks.jsx
@@ -129,6 +129,51 @@ const Tasks = () => {
     document.body.removeChild(link);
   };
 
+  const flattenDeviceInfoObject = (obj, keys, prefix = '') => {
+      return keys.reduce((acc, k) => {
+        const pre = prefix + "_" + k;
+        if (obj === null || obj === undefined) {
+          acc[pre] = '';
+          return acc;
+        } else {
+          acc[pre] = obj[k] || '';
+          return acc;
+        }
+      }, {});
+  }
+
+  const exportAllToCSV = () => {
+    if (tasks.length === 0) {
+      console.log("No tasks to export");
+      return;
+    }
+
+    const deviceInfoKeys = ["at_deviceName", "at_serialNumber", "created_at", "fw_id", "fw_task_id"];
+    const flattenedTasks = tasks.map(task => {
+      const flattenedDevice = flattenDeviceInfoObject(task.deviceInfo, deviceInfoKeys, "deviceInfo");
+      return { ...task, ...flattenedDevice };
+    });
+
+    const taskKeys = Object.keys(flattenedTasks[0]);
+    const csvHeader = [...taskKeys, "component_label"].join(",") + "\n";
+    const csvContent = flattenedTasks.map((task) => {
+      const taskValues = taskKeys.map((key) => task[key]);
+      const taskLabel = formatTaskDisplay(task);
+      return [...taskValues, taskLabel].join(",");
+    }).join("\n");
+    const csvData = csvHeader + csvContent;
+  
+    const blob = new Blob([csvData], { type: "text/csv;charset=utf-8;" });
+    const url = URL.createObjectURL(blob);
+    const link = document.createElement("a");
+    link.setAttribute("href", url);
+    link.setAttribute("download", "all_tasks.csv");
+    link.style.visibility = "hidden";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
   const formatTaskDisplay = (task) => {
     if (!task) {
       return "Task data is not available";
@@ -200,9 +245,14 @@ const Tasks = () => {
             </Flex>
           </Box>
           <Box display="flex" justifyContent="space-between" gap="6px">
-            <Button size="xs" leftIcon={<FaPrint />} colorScheme="blue" px={3} py={4} onClick={exportToCSV}>
-              Get Print File
-            </Button>
+            <div style={{ display: "flex", gap: "6px" }}>
+              <Button size="xs" leftIcon={<FaPrint />} colorScheme="blue" px={3} py={4} onClick={exportToCSV}>
+                Get Print File
+              </Button>
+              <Button size="xs" colorScheme="gray" variant="outline" px={3} py={4} onClick={exportAllToCSV}>
+                Export All
+              </Button>
+            </div>
 
             <div style={{ display: "flex", gap: "6px" }}>
               <Button size="xs" colorScheme="gray" variant="outline" px={3} py={4} onClick={() => setSelectedTasks(new Set(tasks.map((task) => task.id)))}>
