ead69a5fefa9c529e5640440cee17be761c3bdb8
Luke <122643224+lukegarrod@users.noreply.github.com>
2025-02-23 18:12:47 +0200

    fix categories overflow

--- src/components/ProjectTaskFilter.jsx
+++ src/components/ProjectTaskFilter.jsx
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react';
-import { Flex, Text, Tag, TagLabel, TagCloseButton, Box } from '@chakra-ui/react';
-import { FaFilter } from 'react-icons/fa';
+import React, { useState, useEffect } from "react";
+import { Flex, Text, Tag, TagLabel, TagCloseButton, Box } from "@chakra-ui/react";
+import { FaFilter } from "react-icons/fa";
 
 const ProjectTaskFilter = ({ taskStatuses, taskCategories, onSelectedStatusesChange, onSelectedCategoriesChange }) => {
   const [selectedStatuses, setSelectedStatuses] = useState([]);
@@ -8,9 +8,9 @@ const ProjectTaskFilter = ({ taskStatuses, taskCategories, onSelectedStatusesCha
 
   const handleFilterTagClick = (id, selectedItems, setSelectedItems) => {
     if (selectedItems.includes(id)) {
-      setSelectedItems(selectedItems.filter(itemId => itemId !== id));
+      setSelectedItems(selectedItems.filter((itemId) => itemId !== id));
     } else {
-      setSelectedItems(prevItems => [...prevItems, id]);
+      setSelectedItems((prevItems) => [...prevItems, id]);
     }
   };
 
@@ -30,20 +30,24 @@ const ProjectTaskFilter = ({ taskStatuses, taskCategories, onSelectedStatusesCha
 
   return (
     <Flex flexDirection="column" width="100vw">
-      <Flex flex="1" alignItems="center" flexDirection="row" width="100%" overflowX="scroll"  sx={{ '&::-webkit-scrollbar': { display: 'none' }, scrollbarWidth: 'none', msOverflowStyle: 'none' }}>
-        <Text size="md">Status:</Text>
-        <Box whiteSpace="nowrap" display="flex">
+      <Flex flex="1" alignItems="flex-start" flexDirection="row" width="100%" overflowX="scroll" sx={{ "&::-webkit-scrollbar": { display: "none" }, scrollbarWidth: "none", msOverflowStyle: "none" }}>
+        <Text size="md" mt={1} width="80px">
+          Status:
+        </Text>
+        <Box flex="1" whiteSpace="normal" display="flex" flexWrap="wrap">
           {taskStatuses.map((taskStatus, index) => (
-            <Tag key={index} size="md" colorScheme={selectedStatuses.includes(taskStatus.id) ? "blue" : "gray"} variant={selectedStatuses.includes(taskStatus.id) ? "solid" : "outline"} m={1} onClick={() => handleStatusTagClick(taskStatus.id)} cursor="pointer" overflowX="auto" whiteSpace="nowrap">
+            <Tag key={index} size="md" colorScheme={selectedStatuses.includes(taskStatus.id) ? "blue" : "gray"} variant={selectedStatuses.includes(taskStatus.id) ? "solid" : "outline"} m={1} onClick={() => handleStatusTagClick(taskStatus.id)} cursor="pointer">
               <TagLabel>{taskStatus.name}</TagLabel>
               {selectedStatuses.includes(taskStatus.id) && <TagCloseButton />}
             </Tag>
           ))}
         </Box>
       </Flex>
-      <Flex flex="1" alignItems="center" flexDirection="row" width="100%" overflowX="scroll" sx={{ '&::-webkit-scrollbar': { display: 'none' }, scrollbarWidth: 'none', msOverflowStyle: 'none' }}>
-        <Text size="md">Category:</Text>
-        <Box whiteSpace="nowrap" display="flex">
+      <Flex flex="1" alignItems="flex-start" flexDirection="row" width="100%" overflowX="scroll" sx={{ "&::-webkit-scrollbar": { display: "none" }, scrollbarWidth: "none", msOverflowStyle: "none" }}>
+        <Text size="md" mt={1} width="80px">
+          Category:
+        </Text>
+        <Box flex="1" whiteSpace="normal" display="flex" flexWrap="wrap">
           {taskCategories.map((taskCategory, index) => (
             <Tag key={index} size="md" colorScheme={selectedCategories.includes(taskCategory.id) ? "blue" : "gray"} variant={selectedCategories.includes(taskCategory.id) ? "solid" : "outline"} m={1} onClick={() => handleCategoryTagClick(taskCategory.id)} cursor="pointer">
               <TagLabel>{taskCategory.name}</TagLabel>
